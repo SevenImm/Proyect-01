e6fa583c23e5da4cccee22945628f76366dd0206
Jeremy <jbjarvis@mac.com>
2023-11-02 20:27:53 -0500

    idk

--- assets/script.js
+++ assets/script.js
@@ -5,7 +5,9 @@ const contributionsDiv = document.getElementById('contributions');
 
 fetchButton.addEventListener('click', () => {
   const username = usernameInput.value;
-
+  const cityName = cityInput.value;
+  console.log(username);
+  console.log(cityName);
   if (username) {
     const githubUrl = `https://api.github.com/users/${username}/events/public`;
 
@@ -14,12 +16,17 @@ fetchButton.addEventListener('click', () => {
       .then(response => response.json())
       .then(contributions => {
         contributionsDiv.innerHTML = ''; // Clear previous contributions
-          console.log(contributions);
+        
+         // Extract the date portion from the datetime string
+        var datetime = contributions[0].created_at;
+        const date = datetime.split('T')[0];
+        console.log(date);
         contributions.forEach(event => {
           const contributionElement = document.createElement('p');
           contributionElement.textContent = `${event.type} at ${event.created_at}`;
           contributionsDiv.appendChild(contributionElement);
         });
+      
       })
       .catch(error => {
         contributionsDiv.textContent = `Failed to retrieve contributions. Error: ${error}`;
@@ -27,8 +34,8 @@ fetchButton.addEventListener('click', () => {
   }});
 
 // test date and city in right now, but we'll have those as inputs from the html 
-var date = '2023-5-05'
-var city = 'Austin'
+// var date = '2023-5-05'
+// var city = 'Austin'
 // This is a temporary api key on a free trial that ends november 16
 const apiKey = 'f7b41a64bf0c4d4386720154230211'
 const weatherUrl = `https://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${city}&dt=${date}`
@@ -38,7 +45,7 @@ fetch(weatherUrl)
     return response.json();
   })
   .then(data => {
-    // Extract the date portion from the datetime string
+   
     var datetime = data.forecast.forecastday[0].date;
     var date = datetime.split('T')[0];
 
@@ -47,3 +54,5 @@ fetch(weatherUrl)
     console.log("Date:", date);
     console.log(weather);
   });
+
+  // console.log(date);
