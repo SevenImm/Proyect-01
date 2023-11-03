2a72ac9651426550a7227e706ff6ea2cc1e2a594
Jeremy <jbjarvis@mac.com>
2023-11-02 21:27:18 -0500

    combined api fetches

--- assets/script.js
+++ assets/script.js
@@ -2,57 +2,63 @@ const fetchButton = document.getElementById('fetchButton');
 const usernameInput = document.getElementById('username');
 const cityInput = document.getElementById('city');
 const contributionsDiv = document.getElementById('contributions');
+const API_KEY = 'f7b41a64bf0c4d4386720154230211'
 
 fetchButton.addEventListener('click', () => {
   const username = usernameInput.value;
   const cityName = cityInput.value;
   console.log(username);
   console.log(cityName);
-  if (username) {
-    const githubUrl = `https://api.github.com/users/${username}/events/public`;
+ 
+  const githubUrl = `https://api.github.com/users/${username}/events/public`;
 
     // Making the GitHub request
     fetch(githubUrl)
       .then(response => response.json())
       .then(contributions => {
-        contributionsDiv.innerHTML = ''; // Clear previous contributions
         
          // Extract the date portion from the datetime string
         var datetime = contributions[0].created_at;
         const date = datetime.split('T')[0];
-        console.log(date);
+        contributionsDiv.innerHTML = ''; // Clear previous contributions
         contributions.forEach(event => {
           const contributionElement = document.createElement('p');
           contributionElement.textContent = `${event.type} at ${event.created_at}`;
           contributionsDiv.appendChild(contributionElement);
         });
-      
+        const weatherUrl = `https://api.weatherapi.com/v1/history.json?key=${API_KEY}&q=${cityName}&dt=${date}`
+        fetch(weatherUrl)
+          .then(response => {
+            return response.json();
+          })
+          .then(data => {
+            // at the moment it returns the average temp of the day, but lots of things can be targetted
+            var weather = (data.forecast.forecastday[0].day.avgtemp_f);
+            console.log(weather);
+        });
       })
       .catch(error => {
         contributionsDiv.textContent = `Failed to retrieve contributions. Error: ${error}`;
       });
-  }});
+    });
 
 // test date and city in right now, but we'll have those as inputs from the html 
 // var date = '2023-5-05'
 // var city = 'Austin'
 // This is a temporary api key on a free trial that ends november 16
-const apiKey = 'f7b41a64bf0c4d4386720154230211'
-const weatherUrl = `https://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${city}&dt=${date}`
+// const apiKey = 'f7b41a64bf0c4d4386720154230211'
+// const weatherUrl = `https://api.weatherapi.com/v1/history.json?key=${apiKey}&q=${city}&dt=${date}`
 
-fetch(weatherUrl)
-  .then(response => {
-    return response.json();
-  })
-  .then(data => {
+// fetch(weatherUrl)
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then(data => {
    
-    var datetime = data.forecast.forecastday[0].date;
-    var date = datetime.split('T')[0];
 
-    // at the moment it returns the average temp of the day, but lots of things can be targetted
-    var weather = (data.forecast.forecastday[0].day.avgtemp_f);
-    console.log("Date:", date);
-    console.log(weather);
-  });
 
-  // console.log(date);
+//     // at the moment it returns the average temp of the day, but lots of things can be targetted
+//     var weather = (data.forecast.forecastday[0].day.avgtemp_f);
+//     console.log(weather);
+//   });
+
